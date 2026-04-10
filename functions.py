9f24b51ed81849f5d1514f0a4737c8dbd28fc10b
Chip Hardy <chiphardy1995@icloud.com>
2026-04-10 02:38:40 -0500

    refactored helper functions
    renammed init_circuit() to create_circuit() and added the option to add classical bits. Added the compile_circuit() function. Refactored run_circuit() function to user compile_circuit() if circuit is not already transpiled, and to return the result of the job directly instead of the job itself..

--- functions.py
+++ functions.py
@@ -3,20 +3,20 @@ from qiskit import QuantumCircuit, transpile
 from qiskit.visualization import plot_histogram, plot_bloch_multivector
 from qiskit_aer import AerSimulator
 
-def init_circuit(num_qubits = 2) -> QuantumCircuit:
+def create_circuit(number_qubits = 2, number_bits = 2) -> QuantumCircuit:
     """ returns a circuit with the number of given qubits """
-    return QuantumCircuit(num_qubits)
+    return QuantumCircuit(number_qubits, number_bits)
 
-def run_circuit(circuit: QuantumCircuit, backend: AerSimulator = AerSimulator(), shots: int = 1024) -> Any:
+def compile_circuit(circuit: QuantumCircuit, backend = AerSimulator()) -> QuantumCircuit:
+    " returns a compiled circuit using the given backend"
+    return transpile(circuit, backend)
+
+def run_circuit(circuit: QuantumCircuit, transpiled: bool = True, backend = AerSimulator(), shots = 1024) -> Any:
     """ runs the given circuit on the given backend for the given number of shots and returns the result """
-    # transpile the circuit using the given backend's run method
-    transpiled_circuit = transpile(circuit, backend)
-
-    # run the circuit on the backend for the given number of shots
-    job = backend.run(transpiled_circuit, shots=shots)
-
-    # return the result of the job
-    return job.result()
+    if not transpiled:
+        circuit = compile_circuit(circuit, backend)
+    
+    return backend.run(circuit, shots=shots).result()
 
 def insert_block(circuit: QuantumCircuit, block: QuantumCircuit, qubits: list[int]):
     """ inserts the given block into the given circuit at the given qubits """
