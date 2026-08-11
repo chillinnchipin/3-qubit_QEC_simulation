0b16f31cc75cfc4574a422616e8a40fa47376e09
Chip Hardy <chiphardy1995@icloud.com>
2026-08-11 12:23:40 -0500

    added initial testing outline

---
+++ test_functions.py
@@ -0,0 +1,22 @@
+from functions import *
+
+def test_create_circuit():
+    pass
+
+def test_compile_circuit():
+    pass
+
+def test_run_circuit():
+    pass
+
+def test_insert_block():
+    pass
+
+def test_measure_all():
+    pass
+
+def test_measure():
+    pass
+
+def test_draw_circuit():
+    pass
