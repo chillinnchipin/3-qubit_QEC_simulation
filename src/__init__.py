7f98067787786809648e67ef289f21c97e384911
Chip Hardy <chiphardy1995@icloud.com>
2026-09-24 00:17:02 -0500

    wip: quick fix __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 from .utils import set_debug_mode, set_verbose_outputs
 
 # initializes the module with debug mode and verbose set to false, will change later if arguments specify
-set_debug_mode(debug=False)
-set_verbose_output(verbose=False)
+set_debug_mode(to_set=False)
+set_verbose_outputs(to_set=False)
